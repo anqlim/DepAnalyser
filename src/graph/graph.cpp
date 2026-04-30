44bfddf4a6ec9d46cbb4cfc53b3dd84d84220c14
annanaboykina@gmail.com <annanaboykina@gmail.com>
2026-04-30 19:57:58 +0300

    Init graph structure

--- src/graph/graph.cpp
+++ src/graph/graph.cpp
@@ -1,4 +1,25 @@
 #include "graph.h"
 
-Node::Node(std::string file_name_) : file_name(file_name_) {};
+Vertex* Graph::addVertex(const std::string& file_name) {
+    auto vertex = std::make_unique<Vertex>(file_name);
+    Vertex* ptr = vertex.get();
+    vertices_[file_name] = std::move(vertex);
+    return ptr;
+}
 
+Vertex* Graph::findVertex(const std::string& file_name) {
+    auto it = vertices_.find(file_name);
+    if (it != vertices_.end()) {
+        return it->second.get();
+    }
+    else {
+        return nullptr;
+    }
+}
+
+void Graph::addEdge(Vertex* from, Vertex* to) {
+    if (!from || !to) return;
+
+    from->dependents.insert(to);
+    to->dependencies.insert(from);
+}
--- src/graph/graph.h
+++ src/graph/graph.h
@@ -3,21 +3,25 @@
 
 #include <vector>
 #include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <memory>
 
-class Node {
-public:
-    Node(std::string file_name_);
-    ~Node();
+struct Vertex {
+    explicit Vertex(std::string file_name) : file_name(std::move(file_name)) {}
 
     std::string file_name;
-    std::vector<Node*> dependencies;
-    std::vector<Node*> dependents;
-
+    std::unordered_set<Vertex*> dependencies;
+    std::unordered_set<Vertex*> dependents;
 };
 
 class Graph {
-
+private:
+    std::unordered_map<std::string, std::unique_ptr<Vertex>> vertices_;
+public:
+    Vertex* addVertex(const std::string& file_name);
+    Vertex* findVertex(const std::string& file_name);
+    void addEdge(Vertex* from, Vertex* to);
 };
-
 
 #endif
